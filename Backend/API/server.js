7145850e661a52a62afbc1f9d173f6914083ebb4
Bruno <brunosilvalustosa4@gmail.com>
2024-12-15 14:51:49 -0300

    Adicionando Cors

--- Backend/API/server.js
+++ Backend/API/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const routes = require('./routes/index');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  app.use(cors());
+  next();
+});
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 3000;
